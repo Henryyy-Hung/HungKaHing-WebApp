2ad6cf9d405906ee9165c60644f0faa7d035564a
Henry <henryyy@connect.hku.hk>
2024-09-02 06:28:53 +0800

    update

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,13 @@
+import style from './page.module.css';
+
+const BlogsPage = () => {
+    return (
+        <div className={style.container}>
+            <div className={style.card}>
+                <h5>Blogs Page in progress</h5>
+            </div>
+        </div>
+    );
+}
+
+export default BlogsPage;
---
+++ src/app/resume/page.jsx
@@ -0,0 +1,13 @@
+import style from './page.module.css';
+
+const ResumePage = () => {
+    return (
+      <div className={style.container}>
+          <div className={style.card}>
+              <h5>Resume Page in progress</h5>
+          </div>
+      </div>
+    );
+}
+
+export default ResumePage;
